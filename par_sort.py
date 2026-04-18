e4e29b595a404c7e7863396721ea64ceff380371
Carlos Jorge Gamale <gamale.carlos124@gmail.com>
2026-04-18 10:56:31 +0000

    initialize par_sort.py

--- par_sort.py
+++ par_sort.py
@@ -1,1 +1,16 @@
 #Jorgie
+# Parallel Merge Sort
+
+from multiprocessing import Pool
+from seq_sort import sequential_merge_sort, merge
+
+def parallel_merge_sort(arr):
+    if len(arr) <= 1000:
+        return sequential_merge_sort(arr)
+
+    mid = len(arr) // 2
+
+    with Pool(2) as pool:
+        left, right = pool.map(sequential_merge_sort, [arr[:mid], arr[mid:]])
+
+    return merge(left, right)
