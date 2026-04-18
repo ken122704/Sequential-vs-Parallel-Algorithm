6181c19b1b50302b68f6e358dd17ef24be6f8b22
Ken Charles Besa <kencharlesbesa27@gmail.com>
2026-04-18 11:08:30 +0000

    Initialized the datasets

--- dataset.py
+++ dataset.py
@@ -1,1 +1,16 @@
 #ken
+#dataset
+import random
+
+def generate_data(n):
+    return [random.randint(1, 1000000) for _ in range(n)]
+
+
+def generate_sorted_data(n):
+    data = generate_data(n)
+    return sorted(data)
+
+
+def generate_reverse_sorted_data(n):
+    data = generate_data(n)
+    return sorted(data, reverse=True)
